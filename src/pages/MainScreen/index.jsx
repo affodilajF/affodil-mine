209981f019a83f3ac9497f78d22467f682db00a2
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2024-05-26 18:10:09 +0700

    add blur pink

--- src/pages/MainScreen/index.jsx
+++ src/pages/MainScreen/index.jsx
@@ -40,6 +40,8 @@ const Screen = () => {
 
       <div className="circlePosition w-[420px] h-[300px] bg-bgpurple-2 rounded-full absolute top-[550px] left-[-200px] blur-[90px]"></div>
 
+      <div className="circlePosition h-[700px]  bg-bgpurple-5 rounded-full absolute top-[1550px] right-[150] blur-[90px] sm:w-[580px] md:w-[820px]"></div>
+
       <div className="circlePosition w-[520px] h-[600px] bg-bgpurple-1 rounded-full absolute top-[2000px] left-[-300px] blur-[90px]"></div>
 
       <div className="circlePosition  h-[400px] bg-bgpurple-3 rounded-full absolute top-[1800px] left-[100px] blur-[90px] sm:w-[380px] md:w-[420px]"></div>
