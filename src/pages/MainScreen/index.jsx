5ca5bf5b34970a81fad6c382ad5507da71fff3aa
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2024-05-26 16:54:44 +0700

    add desc

--- src/pages/MainScreen/index.jsx
+++ src/pages/MainScreen/index.jsx
@@ -68,9 +68,9 @@ const Screen = () => {
               </span>
               <br></br>
               <br></br>
-              However, my curiosity and eagerness to learn extend far beyond
-              these areas. I am eager to embrace new opportunities and excel in
-              diverse domains.
+              Yet, my thirst for knowledge extends beyond these realms. I am
+              driven to explore diverse domains, eager to seize new
+              opportunities and excel in varied fields. As a growing interest in Data Analysis and Software Testing. 
             </a>
           </div>
           <div className="mt-5">
